c5d4360c708db61d8d27ad54462d724764fed8f6
Seth Wolfgang <sethwolfgang@pop-os.localdomain>
2024-02-12 22:07:01 -0500

    fixed bug for rectangular matrices?

--- nmf.cpp
+++ nmf.cpp
@@ -3,139 +3,178 @@
 #include <vector>
 #include <omp.h>
 #include <cmath>
+#include <fstream>
 // #include "nmf.hpp"
 
 #define TINY_NUM 1e-15
 
 struct NMFResult {
-  Eigen::MatrixXd w;
-  Eigen::VectorXd d;
-  Eigen::MatrixXd h;
+    Eigen::MatrixXd w;
+    Eigen::VectorXd d;
+    Eigen::MatrixXd h;
 };
 
 template <typename T>
 double mean(const T& mat) {
-  return mat.sum() / mat.size();
+    return mat.sum() / mat.size();
 }
 
 // scale rows in w (or h) to sum to 1 and put previous rowsums in d
 void scale(Eigen::MatrixXd& w, Eigen::VectorXd& d) {
-  d = w.rowwise().sum();
-  d.array() += 1e-15;
-  for (long int i = 0; i < w.rows(); ++i)
-    for (long int j = 0; j < w.cols(); ++j) w(i, j) /= d(i);
+    d = w.rowwise().sum();
+    d.array() += 1e-15;
+    for (long int i = 0; i < w.rows(); ++i)
+        for (long int j = 0; j < w.cols(); ++j) w(i, j) /= d(i);
 };
 
 
 // sequential coordinate descent for NMF with KL-divergence (need to add regularization)
-void scd_kl_update(Eigen::VectorXd& hj, const Eigen::MatrixXd& Wt, const Eigen::VectorXd& aj, const Eigen::VectorXd& sumW, const unsigned int max_iter, const double rel_tol) {
+void scd_kl_update(Eigen::VectorXd& hj, const Eigen::MatrixXd& Wt, const Eigen::VectorXd& Aj, const Eigen::VectorXd& sumW, const unsigned int max_iter, const double rel_tol) {
 
-  double sumHj = hj.sum();
-  Eigen::VectorXd Ajt = Wt.transpose() * hj;
-  Eigen::VectorXd mu;
-  double a; // second derivative
-  double b; // first derivative
-  double tmp, etmp;
-  double rel_err = 1 + rel_tol;
+    double sumHj = hj.sum();
+    Eigen::VectorXd Ajt = Wt.transpose() * hj;
+    Eigen::VectorXd mu;
+    double a; // second derivative
+    double b; // first derivative
+    double tmp, etmp;
+    double rel_err = 1 + rel_tol;
 
-  unsigned int t = 0;
+    unsigned int t = 0;
 
-  for (; t < max_iter && rel_err > rel_tol; t++)
-  {
-    rel_err = 0;
-    for (long int k = 0; k < Wt.rows(); k++) {
-      mu = Wt.row(k).transpose().array() / (Ajt.array() + TINY_NUM);
-      // update a to the dot product of aj and mu squared
-      //square mu
-      Eigen::VectorXd mu_squared = mu.array() * mu.array();
-      a = aj.dot(mu_squared);
-      b = aj.dot(mu) - sumW(k);
-      // a += beta(0);
-      // b += a * Hj(k) - beta(2) - beta(1) * (sumHj - Hj(k));
-      tmp = b/(a + TINY_NUM);
-      if (tmp < 0) tmp = 0;
-      if (tmp != hj(k)) {
-        Ajt += (tmp - hj(k)) * Wt.row(k).transpose();
-        etmp = 2 * std::abs(tmp - hj(k)) / (tmp + hj(k) + TINY_NUM);
-        if (etmp > rel_err) rel_err = etmp;
-        sumHj += tmp - hj(k);
-        hj(k) = tmp;
-      }
+    for (; t < max_iter && rel_err > rel_tol; t++) {
+        rel_err = 0;
+        for (long int k = 0; k < Wt.rows(); k++) {
+            mu = Wt.row(k).transpose().array() / (Ajt.array() + TINY_NUM);
+            // update a to the dot product of Aj and mu squared
+            //square mu
+            a = Aj.dot(mu.cwiseProduct(mu));
+            b = Aj.dot(mu) - sumW(k);
+            // a += beta(0);
+            // b += a * Hj(k) - beta(2) - beta(1) * (sumHj - Hj(k));
+            tmp = b / (a + TINY_NUM);
+            if (tmp < 0) tmp = 0;
+            if (tmp != hj(k)) {
+                Ajt += (tmp - hj(k)) * Wt.row(k).transpose();
+                etmp = 2 * std::abs(tmp - hj(k)) / (tmp + hj(k) + TINY_NUM);
+                if (etmp > rel_err) rel_err = etmp;
+                sumHj += tmp - hj(k);
+                hj(k) = tmp;
+            }
+        }
     }
-  }
-  // return t;
+    // return t;
 }
 
 // update h given A and w
-inline void update(Eigen::MatrixXd A, const Eigen::MatrixXd& h, Eigen::MatrixXd& wt, const double L1, const double L2, const int threads) {
-  
-  unsigned int max_iter = 100;
-  double rel_tol = 1e-4;
+inline void update(const Eigen::MatrixXd& h, const Eigen::MatrixXd& wt, Eigen::MatrixXd A, const double L1, const double L2, const int threads) {
 
-  #ifdef _OPENMP
-  #pragma omp parallel for num_threads(threads) schedule(dynamic)
-  #endif
-  for (long int i = 0; i < h.cols(); ++i) {
-    // get sum of W
-    Eigen::VectorXd sumW = wt.rowwise().sum();
-    // get the ith column of A
-    Eigen::VectorXd aj = A.col(i);
-    // get the ith column of H
-    Eigen::VectorXd hj = h.col(i);
-    // update based on kl-divergence
-    scd_kl_update(hj, wt, aj, sumW, max_iter, rel_tol);
-  }
+    unsigned int max_iter = 100;
+    double rel_tol = 1e-4;
+
+    #ifdef _OPENMP
+    #pragma omp parallel for num_threads(threads) schedule(dynamic)
+    #endif
+    for (long int i = 0; i < h.cols(); ++i) {
+        // get sum of W
+        Eigen::VectorXd sumW = wt.rowwise().sum();
+        // get the ith column of A
+        Eigen::VectorXd Aj = A.col(i);
+        // get the ith column of H
+        Eigen::VectorXd hj = h.col(i);
+        // update based on kl-divergence
+        scd_kl_update(hj, wt, Aj, sumW, max_iter, rel_tol);
+    }
 }
 
 NMFResult c_nmf(const Eigen::MatrixXd& A, const Eigen::MatrixXd& At, const unsigned int k, const unsigned int maxit, const double tol, const double L1_h, const double L2_h, const double L1_w, const double L2_w, const unsigned int threads, const unsigned int seed) {
-  
-  
-  // initialize random W and H
-  Eigen::MatrixXd w = Eigen::MatrixXd::Random(k, A.rows());
-  Eigen::MatrixXd h = Eigen::MatrixXd::Zero(w.rows(), A.cols());
-  Eigen::VectorXd d = Eigen::VectorXd::Ones(w.rows());
 
-  double rel_err = tol + 1;
-  double klerror_last = 1e99;
-  std::vector<double> kl_error(maxit);
-  // initialize kl_error to zero
-  for (size_t i = 0; i < maxit; ++i) kl_error[i] = 0;
 
-  for (uint16_t iter_ = 0; iter_ < maxit && rel_err > tol; ++iter_) {
-    // update h
-    update(A, w, h, L1_h, L2_h, threads);
-    scale(h, d);
+    // initialize random W and H
+    Eigen::MatrixXd w = Eigen::MatrixXd::Random(k, A.rows()); // 100 x 10
+    Eigen::MatrixXd h = Eigen::MatrixXd::Zero(k, A.cols()); // 10 x 50
+    Eigen::VectorXd d = Eigen::VectorXd::Ones(w.rows()); // 100 x 1
 
-    // update w
-    update(At, h, w, L1_w, L2_w, threads);
-    scale(w, d);
+    double rel_err = tol + 1;
+    double klerror_last = 1e99;
+    std::vector<double> kl_error(maxit);
+    // initialize kl_error to zero
+    for (size_t i = 0; i < maxit; ++i) kl_error[i] = 0;
 
-    // calculate fit
-    Eigen::MatrixXd A_hat = w.transpose() * h;
-    kl_error[iter_] += ((-(A.array() + TINY_NUM).log() * (A_hat.array() + TINY_NUM) + A_hat.array()).mean());
+    for (uint16_t iter_ = 0; iter_ < maxit && rel_err > tol; ++iter_) {
+        // update w
+        update(w, h, At, L1_w, L2_w, threads);
+        scale(w, d);
+        std::cout << "updated W" << std::endl;
+        // update h
+        update(h, w, A, L1_h, L2_h, threads);
+        scale(h, d);
+        std::cout << "updated H" << std::endl;
 
-    // calculate relative error
-    rel_err = std::abs((klerror_last - kl_error[iter_]) / (klerror_last + kl_error[iter_] + TINY_NUM));
-    klerror_last = kl_error[iter_];
-  }
+        // calculate fit
+        Eigen::MatrixXd A_hat = w.transpose() * h;
+        kl_error[iter_] += ((-(A.array() + TINY_NUM).log() * (A_hat.array() + TINY_NUM) + A_hat.array()).mean());
 
-  return {w, d, h};
+        // calculate relative error
+        rel_err = std::abs((klerror_last - kl_error[iter_]) / (klerror_last + kl_error[iter_] + TINY_NUM));
+        klerror_last = kl_error[iter_];
+
+    }
+
+    return { w, d, h };
 }
 
 
-int main()
-{
+void readCSV(std::string filename, Eigen::Matrix<double, -1, -1>& A) {
+
+    std::vector<std::vector<double>> data;
+    data.reserve(1183);
+    std::ifstream file(filename);
+    std::string line;
+    while (std::getline(file, line)) {
+        std::vector<double> row;
+        row.reserve(184);
+        std::stringstream ss(line);
+        std::string cell;
+        while (std::getline(ss, cell, ',')) {
+            row.push_back(atof(cell.c_str()));
+        }
+        data.push_back(row);
+    }
+    std::cout << "Read " << data.size() << " rows and " << data[0].size() << " columns" << std::endl;
+
+    A = Eigen::Matrix<double, -1, -1>(185, 1183);
+    for (int i = 0; i < data.size(); i++) {
+        for (int j = 0; j < data[i].size(); j++) {
+
+            A(i, j) = data[i][j];
+
+        }
+
+    }
+
+}
+
+
+int main() {
     std::cout << "Hello, world!" << std::endl;
-    
-    
+
+
     // run a simple test
-    Eigen::MatrixXd A = Eigen::MatrixXd::Random(100, 100);
+    // Eigen::MatrixXd A = Eigen::MatrixXd::Random(50, 100);
+    Eigen::Matrix<double, -1, -1> A;
+    readCSV("birds.csv", A);
+
     A = A.cwiseAbs();
 
     Eigen::MatrixXd At = A.transpose();
 
     NMFResult result = c_nmf(A, At, 10, 100, 1e-4, 0, 0, 0, 0, 0, 1);
-    
-    
+
+    // std::cout << "W: " << result.w << std::endl;
+    // std::cout << "D: " << result.d << std::endl;
+    // std::cout << "H: " << result.h << std::endl;
+
+    std::cout << "Res: " << (A - (result.w.transpose() * result.h)).norm() << std::endl;
+
     return 0;
 }
--- nmf.hpp
+++ nmf.hpp
@@ -7,7 +7,7 @@
 template <class T>
 class nmf {
 
-  private:
+    private:
     // Original matrix (and transpose)
     T& A;
     T t_A;
@@ -24,7 +24,7 @@ class nmf {
     double tol_ = -1;
     unsigned int iter_ = 0, best_model_ = 0;
 
-   public:
+    public:
     unsigned int maxit = 100;
     unsigned int threads = 0;
     std::vector<double> L1 = std::vector<double>(2);
@@ -34,9 +34,9 @@ class nmf {
 
     // Constructors
     nmf(T& A, const unsigned int k, const unsigned int seed = 0) : A(A) {
-      W = randomMatrix(k, A.rows(), seed);
-      H = Eigen::MatrixXd(k, A.cols());
-      D = Eigen::VectorXd::Ones(k);
+        W = randomMatrix(k, A.rows(), seed);
+        H = Eigen::MatrixXd(k, A.cols());
+        D = Eigen::VectorXd::Ones(k);
     }
 
 
@@ -53,49 +53,49 @@ class nmf {
 
     // scale rows in "w" to sum to 1, where "d" is rowsums of "w"
     void scaleW() {
-      D = W.rowwise().sum();
-      D.array() += TINY_NUM;
-      for (unsigned int i = 0; i < W.rows(); ++i)
-        for (unsigned int j = 0; j < W.cols(); ++j) W(i, j) /= D(i);
+        D = W.rowwise().sum();
+        D.array() += TINY_NUM;
+        for (unsigned int i = 0; i < W.rows(); ++i)
+            for (unsigned int j = 0; j < W.cols(); ++j) W(i, j) /= D(i);
     };
 
     // scale rows in "h" to sum to 1, where "d" is rowsums of "h"
     void scaleH() {
-      D = H.rowwise().sum();
-      D.array() += TINY_NUM;
-      for (unsigned int i = 0; i < H.rows(); ++i)
-        for (unsigned int j = 0; j < H.cols(); ++j) H(i, j) /= D(i);
+        D = H.rowwise().sum();
+        D.array() += TINY_NUM;
+        for (unsigned int i = 0; i < H.rows(); ++i)
+            for (unsigned int j = 0; j < H.cols(); ++j) H(i, j) /= D(i);
     };
 
     // project "w" onto "A" to solve for "h" in the equation "A = wh"
     void predictH() {
-      predict(A, W, H, L1[1], L2[1], threads);
+        predict(A, W, H, L1[1], L2[1], threads);
     }
 
     // project "h" onto "t(A)" to solve for "w"
     void predictW() {
-      if (symmetric)
-        predict(A, H, W, L1[0], L2[0], threads);
-      else {
-        if (!transposed) {
-          t_A = A.transpose();
-          transposed = true;
+        if (symmetric)
+            predict(A, H, W, L1[0], L2[0], threads);
+        else {
+            if (!transposed) {
+                t_A = A.transpose();
+                transposed = true;
+            }
+            predict(t_A, H, W, L1[0], L2[0], threads);
         }
-        predict(t_A, H, W, L1[0], L2[0], threads);
-      }
     };
 
     void fit() {
-      // alternating least squares updates
-      for (; iter_ < maxit; ++iter_) {
-        Eigen::MatrixXd W_it = W;
-        predictH();  // update "h"
-        scaleH();
-        predictW();  // update "w"
-        scaleW();
-        tol_ = cor(W, W_it);  // correlation between "w" across consecutive iterations
-        if (tol_ < tol) break;
-      }
+        // alternating least squares updates
+        for (; iter_ < maxit; ++iter_) {
+            Eigen::MatrixXd W_it = W;
+            predictH();  // update "h"
+            scaleH();
+            predictW();  // update "w"
+            scaleW();
+            tol_ = cor(W, W_it);  // correlation between "w" across consecutive iterations
+            if (tol_ < tol) break;
+        }
     }
 
     // Helper functions
@@ -103,13 +103,16 @@ class nmf {
     Eigen::MatrixXd randomMatrix(const unsigned int nrow,
                                  const unsigned int ncol,
                                  const unsigned int seed) {
-      std::vector<double> random_values = getRandomValues(nrow * ncol, seed);
-      Eigen::MatrixXd x(nrow, ncol);
-      unsigned int indx = 0;
-      for (unsigned int r = 0; r < nrow; ++r)
-        for (unsigned int c = 0; c < ncol; ++c, ++indx)
-          x(r, c) = random_values[indx];
-      return x;
+        std::vector<double> random_values = getRandomValues(nrow * ncol, seed);
+        Eigen::MatrixXd x(nrow, ncol);
+        unsigned int indx = 0;
+        for (unsigned int r = 0; r < nrow; ++r)
+            for (unsigned int c = 0; c < ncol; ++c, ++indx)
+                x(r, c) = random_values[indx];
+        return x;
     }
+
+
+
 
 };
